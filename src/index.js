bc97ce351ccab7a295c56213e7ee32d0569653a9
James <jameslabroy@gmail.com>
2018-07-24 22:20:04 +0100

    fixed routing

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "./redux/configure-store";
 import routes from "./routing";
 import Layout from "./views/layouts/base";
 import registerServiceWorker from "./registerServiceWorker";
 
+const App = withRouter(() => (
+  <Layout>
+    {routes.map(({ path, ...rest }) => (
+      <Route
+        path={path}
+        key={`route-${path}`}
+        {...rest}
+      />
+    ))}
+  </Layout>
+));
+
 ReactDOM.render(
   <Provider store={configureStore}>
     <BrowserRouter>
-      <Layout>
-        {routes.map(({ path, ...rest }) => (
-          <Route
-            path={path}
-            key={`route-${path}`}
-            {...rest}
-          />
-        ))}
-      </Layout>
+      <App />
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
